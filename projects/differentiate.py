f6af5ecf3468c7edea3c9ccacb0070e7f0918a5f
simonled <simon.lederhilger@gmail.com>
2024-08-29 18:13:13 +0200

    Passed

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -1,11 +1,21 @@
 import numpy as np
 
-
 def differentiate(u, dt):
-    pass
+    u_len = len(u)
+    d = np.zeros(u_len)
+    for i in range(1, u_len - 1):
+        d[i] = (u[i+1] - u[i-1])/(2*dt)
+    frwrd = (u[1] - u[0])/dt; bckwrd = (u[u_len - 1] - u[u_len - 2])/dt
+    d[0] = frwrd; d[-1] = bckwrd
+    return d
 
 def differentiate_vector(u, dt):
-    pass
+    u_len = len(u)
+    d = np.zeros(u_len)
+    d[1:u_len-1] = (u[2:u_len] - u[0:u_len-2])/(2*dt)
+    frwrd = (u[1] - u[0])/dt; bckwrd = (u[u_len - 1] - u[u_len - 2])/dt
+    d[0] = frwrd; d[-1] = bckwrd
+    return d
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
@@ -17,5 +27,3 @@ def test_differentiate():
 
 if __name__ == '__main__':
     test_differentiate()
-    
-    
--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -1,14 +1,21 @@
-import numpy as np
-
+from numpy import exp, array, allclose, zeros_like
 
 def mesh_function(f, t):
-    pass
+    mesh_function = zeros_like(t, dtype = float)
+    for i in range(len(t)):
+        mesh_function[i] = f(t[i])
+    return mesh_function
 
 def func(t):
-    pass
+    if t >= 0 and t <= 3:
+        return exp(-t)
+    elif t > 3 and t <= 4:
+        return exp(-3*t)
+    else:
+        print("Domain not vlaid."); quit()
 
 def test_mesh_function():
-    t = np.array([1, 2, 3, 4])
-    f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
+    t = array([1, 2, 3, 4])
+    f = array([exp(-1), exp(-2), exp(-3), exp(-12)])
     fun = mesh_function(func, t)
-    assert np.allclose(fun, f)
+    assert allclose(fun, f)
